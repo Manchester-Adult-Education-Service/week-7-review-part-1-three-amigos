7582af904a004bb2bb253bf1ab331ee8afceaee2
821945-hub <821945@manadulted.ac.uk>
2025-10-28 19:20:31 +0000

    Completed task 3

--- Ex1_review.py
+++ Ex1_review.py
@@ -114,7 +114,14 @@ print("-------------------------------------------\n"
 # You are an adult!
 #
 # Write your code below:
-
+if age >= 61 and age <= 79:
+    print("You are a Boomer!")
+elif age >= 45 and age <= 60:
+    prin("You are Gen X!")    
+elif age >= 29 and age <= 44:
+    print("You are a Millenial!")
+else:
+    print("You are not a Boomer, Millenial or Gen X")
 
 # -------------------------------------------
 # SWAP COMPUTERS
